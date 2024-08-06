eb3b76e5ac7cddc7c96ad2dfa94b2eeaff5ca397
webstyle707 <webstyle707@gmail.com>
2024-08-06 15:12:17 +0800

    update

--- script.js
+++ script.js
@@ -55,5 +55,6 @@ const hiddenMenu = ()=>{
 
 function url() {
   fbq("track", "Purchase");
+  fbq("track", "SubmitApplication");
   location.href = "https://chat.whatsapp.com/DEwTmeThw93BqGjowUxCfE";
 }
